3718638582637cb107bdd18fe07835b08f92108b
Chen2908 <chengal@post.bgu.ac.il>
2019-12-12 21:41:33 +0200

    manager functions

--- src/Manager.java
+++ src/Manager.java
@@ -1,27 +1,53 @@
+import java.io.File;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Manager {
 
     private static final int BATCH_SIZE = 2000;
+    private static final int THRESHOLD = 2 ;
+
     private ReadFile reader;
     private String corpusPath;
     private String postingPath ;
     private Parse parser;
     private Indexer inverter;
-    private HashMap<String, Term> allTerms;
+    private String dictionaryPath;
+    private String indexPath;
+    private boolean stemming;
+    private int corpusSize;
 
 
     public Manager(String corpusPath, String postingPath, boolean stemming) {
-        this.reader = new ReadFile(corpusPath);
         this.corpusPath = corpusPath;
         this.postingPath = postingPath;
+        this.stemming = stemming;
+        this.reader = new ReadFile(corpusPath);
         this.parser = new Parse(corpusPath, stemming);
-        this.inverter = new Indexer();
-
-        //init paths
-
+        setPaths();
+        this.corpusSize= findCorpusSize(corpusPath);
+       // this.inverter = new Indexer(this.indexPath, corpusSize, THRESHOLD);
     }
+
+
+    private void setPaths() {
+        String path = createIndexFolders();
+        this.dictionaryPath = path + "/dictionary.txt";
+        this.indexPath = path + "Index";
+    }
+
+    private static int findCorpusSize(String corpusPath) {
+        int countDocs = 0;
+        ReadFile reader = new ReadFile(corpusPath);
+        Document doc = reader.getNextDoc();
+        while (doc!=null){
+            countDocs++;
+            doc = reader.getNextDoc();
+        }
+        return countDocs;
+    }
+
 
     public void callReaderAndParser(){
         HashMap<String, Term> docTerms;
@@ -35,6 +61,29 @@ public class Manager {
 
     private void callIndexBuild(HashMap<String,Term> docTerms) {
 
+    }
+
+    public String getDictionaryPath(){
+        return dictionaryPath;
+    }
+
+    private String createIndexFolders() {
+        String stem;
+        if(stemming){
+            stem = "With Stemming";
+        }
+        else {
+            stem = "Without Stemming";
+        }
+        String dir = postingPath + stem;
+        File mainFolder = new File(dir);
+        if (!mainFolder.exists()) {
+            mainFolder.mkdir();
+            File indexFolder = new File(dir + "/Index");
+            if (!indexFolder.exists())
+                indexFolder.mkdir();
+        }
+        return dir;
     }
 
 
